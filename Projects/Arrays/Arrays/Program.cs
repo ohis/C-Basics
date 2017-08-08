a49822d2e3d485db59b8c3b5eca2753329205dce
ohis <ohifemeigbarumah1@gmail.com>
2017-08-07 21:01:30 -0500

    added arrays

---
+++ Projects/Arrays/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[2];//the 2 represents the size of the array
+            names[0] = "John Doe";
+            names[1] = "Jane Doe";
+
+            foreach (string s in names)
+                Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
